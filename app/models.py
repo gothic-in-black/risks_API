d9f45b16449e7814b365df8868ce2e34f0b33f7b
Natalia Pavlova <gothic-in-black@mail.ru>
2025-04-01 15:21:28 +0300

    The database table information has been updated to the latest version

--- app/models.py
+++ app/models.py
@@ -6,25 +6,27 @@ class Patients(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     birthday = db.Column(db.Date, nullable=False)
-    sex = db.Column(db.String(8), nullable=False)
+    gender = db.Column(db.String(8), nullable=False)
     snils = db.Column(db.String, nullable=False)
     id_firm = db.Column(db.Integer, nullable=False)
-    smoking = db.Column(db.Boolean, nullable=False)
 
 
 class Research(db.Model):
     __tablename__ = 'research'
     id = db.Column(db.Integer, primary_key=True)
     id_type = db.Column(db.Integer, nullable=False)
-    date = db.Column(db.Date, nullable=False)
     id_patient = db.Column(db.Integer, nullable=False)
-    name = db.Column(db.String(100), nullable=False)
-    birthday = db.Column(db.Date, nullable=False)
-    sex = db.Column(db.String(8), nullable=False)
-    smoking = db.Column(db.Boolean, nullable=False)
-    cholesterol = db.Column(db.Float, nullable=False)
-    ad = db.Column(db.Float, nullable=False)
     id_firm = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    birthday = db.Column(db.Date, nullable=False)
+    gender = db.Column(db.String(8), nullable=False)
+    cholesterol = db.Column(db.Float, nullable=True)
+    blood_pressure = db.Column(db.SmallInteger, nullable=True)
+    smoking = db.Column(db.SmallInteger, nullable=False)
+    diastolic_bp = db.Column(db.SmallInteger, nullable=True)
+    systolic_bp = db.Column(db.SmallInteger, nullable=True)
+    pulse = db.Column(db.SmallInteger, nullable=True)
 
 
 class Risk(db.Model):
@@ -33,6 +35,7 @@ class Risk(db.Model):
     id_type = db.Column(db.Integer, nullable=False)
     risk = db.Column(db.Float, nullable=False)
     id_patient = db.Column(db.Integer, nullable=False)
-    name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String, nullable=False)
     birthday = db.Column(db.Date, nullable=False)
     id_firm = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
